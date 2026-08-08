b6e14ffd3927fa8f7d5eeabffcabfa09e9822d5d
ruhanisovana <ruhanisovana@gmail.com>
2026-08-08 07:56:31 +0530

    Update inputNode.js

--- src/nodes/inputNode.js
+++ src/nodes/inputNode.js
@@ -19,12 +19,12 @@ export const InputNode = ({ id, data }) => {
   return (
     <BaseNode id={id} title="Input" color="#6b7280" outputs={[{id: `${id}-value`}]}>
       <label style={{display: 'block', marginBottom: 6}}>Name: 
-        <input value={currName} onChange={(e) => {setCurrName(e.target.value); updateData(e.target.value, inputType)}} 
+        <input value={currName} placeholder="Enter Input.." onChange={(e) => {setCurrName(e.target.value); updateData(e.target.value, inputType)}} 
         style={{width: '100%', background: '#0f0f10', border: '1px solid #2a2a2b', color: 'white', borderRadius: 4, padding: 4, marginTop: 4}} />
       </label>
       <label>Type: 
         <select value={inputType} onChange={(e) => {setInputType(e.target.value); updateData(currName, e.target.value)}}
-        style={{width: '100%', background: '#0f0f10', border: '1px solid #2a2a2b', color: 'white', borderRadius: 4, padding: 4, marginTop: 4}}>
+        style={{width: '100%', background: '#0f0f10', border: '1px solid #2a2a2b', color: 'white', borderRadius: 4, padding: 4, marginTop: 4, opacity: 0.9}}>
           <option value="Text">Text</option><option value="File">File</option>
         </select>
       </label>
